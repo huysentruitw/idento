f3dd3fc8a4dede367eaa079a56b16e2b68362f80
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2016-01-05 21:09:33 +0100

    Add GlobalAssemblyInfo.cs

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Idento")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Huysentruit Wouter")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Idento.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // For Moq
