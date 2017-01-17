a775ca107d0127aff2e2f2de1624e00abb423195
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2017-01-17 14:56:41 +0100

    Remove copyright year

--- src/Idento.Domain.Sql/Extensions/ServiceCollectionExtensions.cs
+++ src/Idento.Domain.Sql/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2017 Wouter Huysentruit
+ * Copyright (c) Wouter Huysentruit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
